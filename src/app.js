ff829fa95d70d4cc55b89750c0ef2bcf066e8048
Dolapo Toki <dolapotoki@hotmail.ca>
2018-03-15 17:39:04 +0000

    Work in progress: refactored db connection

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const url = require('url');
-
 const port = process.env.PORT;
-
+const mongoHelper = require("../lib/mongodb");
 const config = require('./config.json');
 const { client_id, client_secret, scope, host } = config;
-
 const redirect_uri = `${host}:${port}/oauth/redirect`;
 const slack_auth_uri = 'https://slack.com/oauth/authorize';
 const slack_access_uri = 'https://slack.com/api/oauth.access';
-
+const assert = require('assert');
 const app = express();
+
+
+async function insertData( { team_id : id , data} = {} ){
+  let result = await mongoHelper.connectToMongo();
+  result.collection("secrets").insertOne({ "_id": id, "data":data }).then((result) =>{
+    console.log(result);
+  }).catch((e)=>{
+    console.error(e);
+  });
+}
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/oauth/authorize', (req, res) => {
@@ -30,7 +39,7 @@ app.get('/oauth/authorize', (req, res) => {
   res.redirect(auth_url);
 });
 
-let secrets = { ok: false };
+
 function requestAccess(code) {
   const params = {
     client_id,
@@ -46,7 +55,8 @@ function requestAccess(code) {
   fetch(access_url)
     .then(res => res.json())
     .then(res => {
-      secrets = res;
+      let {team_id} = res;
+      insertData(team_id, res);
       return res;
     });
 }
@@ -59,7 +69,13 @@ app.get('/oauth/redirect', (req, res) => {
 
 app.post('/api/command', (req, res) => {
   console.log(req.body);
-  res.status(200).json({ text: 'It works' });
+  getMongoClent().collection("secrets").findOne({ "_id": req.body.team_id }).then( result => {
+    console.log(result.data);
+    res.status(200).json({ "text": JSON.stringify(result.data) });
+  }).catch(e => {
+    console.error(e);
+  });
+  
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
